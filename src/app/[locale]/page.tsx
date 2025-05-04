5d3c6554f333524eaaac7728d88d8ba344248f63
Vladimir Lebiga <lebiga@Vladimirs-MacBook-Pro.local>
2025-05-04 16:48:45 +0200

    vercel fix

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,7 +2,7 @@ import { notFound } from 'next/navigation';
 import { supportedLocales } from '@/utils/languageNegotiator';
 import { HomePage } from '@/components/Pages/HomePage';
 
-export default async function Page({ params }: { params: { locale: string } }) {
+export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
   // eslint-disable-next-line @typescript-eslint/await-thenable
   const { locale } = await params;
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { HomePage } from '@/components/Pages/HomePage';
 
-export default function Home() {
+export default async function Page({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params;
   return (
     <main>
       <HomePage />
